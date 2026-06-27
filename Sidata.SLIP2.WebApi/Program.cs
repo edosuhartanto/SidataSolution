762c1107a79a2904ba29a587edc6f5c0167e3f7f
Edo Suhartanto <edosuhartanto@users.noreply.github.com>
2026-06-27 21:12:42 +0700

    try fixing on building openapi json at build (fail)

--- Sidata.SLIP2.WebApi/Program.cs
+++ Sidata.SLIP2.WebApi/Program.cs
@@ -6,53 +6,71 @@
 // ******************************************************
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using Sidata.Abstractions.Queryable.SqlServer.Extensions;
 using Sidata.SLIP2.Data.Context;
 using System.Text.Json.Serialization;
 
-//---------------------------------------------------------------
-//-- Setup Builder
-//---------------------------------------------------------------
-var builder = WebApplication.CreateBuilder(args);
-
-// dataContext
-builder.Services.AddDbContextFactory<LoyaltyDbContext>(options =>
+internal class Program
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddQueryableLikeOperatorForSqlServer();
-
-// Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+    private static void Main(string[] args)
     {
-        options.JsonSerializerOptions.Converters.Add(
-            new JsonStringEnumConverter());
-    });
+        //---------------------------------------------------------------
+        //-- Setup Builder
+        //---------------------------------------------------------------
+        var builder = WebApplication.CreateBuilder(args);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        // dataContext
+        builder.Services.AddDbContextFactory<LoyaltyDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddQueryableLikeOperatorForSqlServer();
 
-//---------------------------------------------------------------
-//-- Build the App
-//---------------------------------------------------------------
-var app = builder.Build();
+        // Add json so endpoint can receive or response Enum in string name
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(
+                    new JsonStringEnumConverter());
+            });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v2", new OpenApiInfo   // <-- tanpa prefix Microsoft.OpenApi.Models
+            {
+                Title = "Sistem Loyalti Pelanggan",
+                Version = "v2",
+                Description = "API utk melayani sistem loyalti pelanggan"
+            });
+        });
+
+        //---------------------------------------------------------------
+        //-- Build the App
+        //---------------------------------------------------------------
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v2/swagger.json", "Sistem Loyalti Pelanggan v2");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        //---------------------------------------------------------------
+        //-- Run it
+        //---------------------------------------------------------------
+        app.Run();
+        }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-//---------------------------------------------------------------
-//-- Run it
-//---------------------------------------------------------------
-app.Run();
